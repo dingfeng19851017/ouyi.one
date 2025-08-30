b943257dcb8f0fc16ad06fda1d2d8a7f80c39a32
dingfeng19851017 <40775745+dingfeng19851017@users.noreply.github.com>
2025-08-30 09:07:06 +0800

    Create download.php

---
+++ includes/download.php
@@ -0,0 +1,3 @@
+<section class="flex flex-col sm:flex-row justify-center items-center gap-4 p-4 rounded-lg shadow-md mx-auto max-w-md">
+  <?php include $_SERVER['DOCUMENT_ROOT'].'/navbtn.php'; ?>
+</section>
